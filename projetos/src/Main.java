38fcc7d4ef0bfe5552a23c619e453377d2677894
Nathali Herpich Valle <nathaliherpichvalle@gmail.com>
2025-08-21 14:33:54 -0300

    Update Main.java

--- projetos/src/Main.java
+++ projetos/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Carro carro = new Carro();
-        carro.name1 = "Porshe";
+        carro.name1 = "Porsche";
         carro.marca1();
         carro.getAnoDeLancamento();
 
